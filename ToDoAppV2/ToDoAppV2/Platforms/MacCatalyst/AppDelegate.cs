b8e1a0bd32572a4981fbd22084b7c68f749f1f1b
suchxs <corsegajoseph@gmail.com>
2026-04-15 21:38:09 +0800

    ToDoAppV2 Initial Commit

---
+++ ToDoAppV2/ToDoAppV2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ToDoAppV2;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
